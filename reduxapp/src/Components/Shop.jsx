0f6d3b8a994eabde2e75a5bd9ed2740bb25d56d0
ronak0447 <109122441+ronak0447@users.noreply.github.com>
2023-03-28 10:27:16 +0530

    28/3

--- reduxapp/src/Components/Shop.jsx
+++ reduxapp/src/Components/Shop.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-        <h2>Buy Adidas Shoes 50 Rs</h2>
+        <h2>Deposit/Withdraw Money</h2>
         <button className="btn btn-primary mx-2">-</button>
         Add to cart
         <button className="btn btn-primary mx-2">+</button>
---
+++ reduxapp/src/state/action-creators/index.js
@@ -0,0 +1,16 @@
+export const depositMoney =(amount) =>{
+    return (dispatch)=>{
+        dispatch:({
+            type: '',
+            payload: amount
+        })
+    }
+}
+export const WithdrawMoney =(amount) =>{
+return (dispatch)=>{
+    dispatch:({
+        type: '',
+        payload: amount
+    })
+}
+}
